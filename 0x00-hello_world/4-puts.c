4a75d384fbaa9558d50f73a69169cf2d17bd1d4c
allan <allancheruiyot003@gmail.com>
2022-12-08 05:25:26 -0800

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 /*Initial description*/
-/**
- * Display text using puts function
- */
 int main(void)
 {
 	/*Print text using put function*/
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 	/*Returns a void*/
+	return (0);
 }
