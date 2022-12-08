d6cc785539598fa07d7a1eb6d47edd38afbfc718
allan <allancheruiyot003@gmail.com>
2022-12-08 13:14:22 -0800

    ALX SE

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 1
  */
 int main(void)
 {
 	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	return (1);
 }
