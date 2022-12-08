7f22181d7026e11a36f4567a42343469c90e7552
allan <allancheruiyot003@gmail.com>
2022-12-08 05:24:00 -0800

    C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*Initial comment*/
+int main(void)
+{
+	/*Using printf*/
+	printf ("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+	/*Returns no value*/
+}
