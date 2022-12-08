7520587b2adb2eb0fc066ba393efbd0c0f65486b
allan <allancheruiyot003@gmail.com>
2022-12-08 05:26:15 -0800

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 	/*Using printf*/
-	printf ("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
 	/*Returns no value*/
+	retu4n (0);
 }
