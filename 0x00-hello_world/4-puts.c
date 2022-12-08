b1b297a4b1889ab9654ea341ebdda4d750e30547
allan <allancheruiyot003@gmail.com>
2022-12-08 02:34:13 -0800

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-int main(){
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+/**
+ *Display text using puts function
+ */
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
