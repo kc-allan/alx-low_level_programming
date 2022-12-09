703225e9d7833d86fb641fe6be4778d53b9b7b45
allan <allancheruiyot003@gmail.com>
2022-12-09 07:45:25 -0800

    ALX SE Comditional statements

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -11,15 +15,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n<0)
+	if (n < 0)
 	{
-		printf("%d is positive", n);
+       		printf("%d is negative\n", n);
 	}
-	else if (n==0)
+	else if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 	else
-		printf("%d is ppositive", n);
+		printf("%d is positive\n", n);
 	return (0);
 }
