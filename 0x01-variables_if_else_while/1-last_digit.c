a9955bb00c7328b96f20abf22218d02122691dd1
allan <allancheruiyot003@gmail.com>
2022-12-09 07:53:58 -0800

    ALX SE Comditional statements

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf("%d\n", n[-1]);
+	return (0);
+}
