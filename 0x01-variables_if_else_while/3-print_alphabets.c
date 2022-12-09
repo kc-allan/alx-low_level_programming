d145534cbf4ec4a26d33718d5218e357a8c75d25
allan <allancheruiyot003@gmail.com>
2022-12-09 10:44:22 -0800

    Conditional Statements

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio>
+/**
+ * maun -Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char small, big;
+	small = 'a';
+	big = 'A';
+
+	while (small <= 'z')
+	{
+		putchar(small);
+		small++;
+	}
+	if (small == 'z')
+	{
+		while (big <= 'Z')
+		{
+			putchar(big);
+			big++;
+		}
+	putchar('\n');
+	return (0);
+}
