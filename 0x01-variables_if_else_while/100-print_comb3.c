920c1ee4e41145460954dbe6443a9e195591b2ab
allan <allancheruiyot003@gmail.com>
2022-12-11 12:11:14 -0800

    Conditional Statements

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,11 @@ int main(void)
 	{
 		for (j = i + 1; j <= '9'; j++)
 		{
-			putchar(i);
-			putchar(j);
+			if (!(i >= j))
+			{
+				putchar(i);
+				putchar(j);
+			}
 			if (!(i == '8' && j == '9'))
 			{
 				putchar(',');
