012c8b4d9955798903a2a6e56a51b33acc28b744
allan <allancheruiyot003@gmail.com>
2022-12-14 12:19:56 -0800

    Functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,28 +1,27 @@
 #include "main.h"
 /**
  * print_to_98 - Prints from parameter a to 98
- * @n: Starting point
+ * @r: Starting point
  * Return: 0
  */
-void print_to_98(int n)
+void printnumber(int n)
 {
-	while (n != 98)
+	if (n < 0)
 	{
-		if (n / 10)
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putcha(',');
-			_putchar(' ');
-		}
-		if (n < 98)
-		{
-			n++;
-		}
-		else
-		{
-			n--;
-		}
+		_putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+		_putchar('0');
+	if (n/10)
+		printnumber(n/10);
+	putchar(n%10 + '0');
+}
+void print_to_98(int r)
+{
+	while (r != 98)
+	{
+		printnumber(r);
 	}
 	_putchar('9');
 	_putchar('8');
