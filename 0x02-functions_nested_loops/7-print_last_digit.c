eaf4a89fdc8611e3445c516fa163dc284ddfbe2e
allan <allancheruiyot003@gmail.com>
2022-12-14 07:13:29 -0800

    Functioms

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - Displays last digit of a number n
+ * @n: parameter number
+ * Return: Value of last digit r
+ */
+int print_last_digit(int n)
+{
+	int r;
+	int d = sizeof(n);
+	for (r = 0; r <= d; r++)
+	{
+	}
+	return (r);
+}
