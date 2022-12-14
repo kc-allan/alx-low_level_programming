6b7fe0229608857efb9ad414493b30f4283e4c59
allan <allancheruiyot003@gmail.com>
2022-12-14 11:44:48 -0800

    Functions

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_to_98 - Prints from parameter a to 98
+ * @n: Starting point
+ * Return: 0
+ */
+void print_to_98(int m)
+{
+	while (n != 98)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		_putchar(',');
+		_putchar(' ');
+		if (n < 98);
+		{
+			n++;
+		}
+		else
+		{
+			n--;
+		}
+	_putchar(98 + '0');
+	}
+}
