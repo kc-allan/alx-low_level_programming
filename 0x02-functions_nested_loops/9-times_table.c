3a44dbc1cdbb833df7fccb21b035620dbcfdee81
allan <allancheruiyot003@gmail.com>
2022-12-14 08:23:15 -0800

    Functions

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table - Displays the 9 by 9 times table
+ * Return: 0
+ */
+void times_table(void)
+{
+	int line, pos;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (pos = 0; pos <= 9; pos++)
+		{
+			if (line == 9);
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(line * pos);
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
