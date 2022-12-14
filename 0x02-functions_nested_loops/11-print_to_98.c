58da0519732b7951f1699eb88b64f320c82c1a7a
allan <allancheruiyot003@gmail.com>
2022-12-14 12:36:09 -0800

    Functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,9 +13,9 @@ void printnumber(int n)
 	}
 	if (n == 0)
 		_putchar('0');
-	if (n/10)
-		printnumber(n/10);
-	_putchar(n%10 + '0');
+	if (n / 10)
+		printnumber(n / 10);
+	_putchar((n % 10) + '0');
 }
 void print_to_98(int r)
 {
@@ -24,13 +24,13 @@ void print_to_98(int r)
 		if (r < 0)
 		{
 			_putchar('-');
-                  	n = -n;
+                  	r = -r;
 		}
-		if (n == 0)
+		if (r == 0)
 			_putchar('0');
-		if (n/10)
-		       	printnumber(n/10);
-		_putchar(n%10 + '0');
+		if (r / 10)
+		       	printnumber(n / 10);
+		_putchar ((n % 10) + '0');
 		r++;
 	}
 	_putchar('9');
