55534a68e0456e5850d48483e55692e41164189d
allan <allancheruiyot003@gmail.com>
2022-12-14 12:46:38 -0800

    Functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,36 +4,20 @@
  * @r: Starting point
  * Return: 0
  */
-void printnumber(int n)
+void print_to_98(int n)
 {
-	if (n < 0)
+	if (n < 98)
 	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n == 0)
-		_putchar('0');
-	if (n / 10)
-		printnumber(n / 10);
-	_putchar((n % 10) + '0');
-}
-void print_to_98(int r)
-{
-	while (r != 98)
-	{
-		if (r < 0)
+		for (;n <= 98; n++)
 		{
-			_putchar('-');
-                  	r = -r;
+			printf(n);
 		}
-		if (r == 0)
-			_putchar('0');
-		if (r / 10)
-		       	printnumber(r / 10);
-		_putchar ((r % 10) + '0');
-	r++;
 	}
-	_putchar('9');
-	_putchar('8');
-	_putchar('\n');
+	else
+	{
+		for (;n >= 98; n--)
+		{
+			printf(n);
+		}
+	}
 }
