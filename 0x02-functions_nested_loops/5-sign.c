bf6511d7e2a6c1418a855d1441331d1a523e1e68
allan <allancheruiyot003@gmail.com>
2022-12-14 06:44:33 -0800

    Functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,14 +8,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return ('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		return ('0');
+		return (0);
 	}
 	else
 	{
-		return ('-');
+		return (-1);
 	}
 }
