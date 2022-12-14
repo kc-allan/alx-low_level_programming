4cf15cf530a48da8f2453ce057d2cfd4e71c29cf
allan <allancheruiyot003@gmail.com>
2022-12-14 06:42:10 -0800

    Functions

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - Checks for the sign of a parameter n, if positive, negative or a zero
+ * @n: value to be checked
+ * Return: 1 (Positive) 0 (Zero) -1 (Negative)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return ('+');
+	}
+	else if (n == 0)
+	{
+		return ('0');
+	}
+	else
+	{
+		return ('-');
+	}
+}
