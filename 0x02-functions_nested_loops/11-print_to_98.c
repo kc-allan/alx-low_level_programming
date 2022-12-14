fda6781b029afed2a00ff6d0035298d733ade525
allan <allancheruiyot003@gmail.com>
2022-12-14 12:51:59 -0800

    Functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,15 +11,15 @@ void print_to_98(int n)
 	{
 		for (;n < 98; n++)
 		{
-			printf("%d, ", n);
+			printf("%d, ", &n);
 		}
 	}
 	else
 	{
 		for (;n > 98; n--)
 		{
-			printf("%d, ", n);
+			printf("%d, ", &n);
 		}
 	}
-	printf(98);
+	printf("%d", 98);
 }
