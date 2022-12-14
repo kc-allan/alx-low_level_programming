7f7ac0817aca5178998862db23ef7f49572e375e
allan <allancheruiyot003@gmail.com>
2022-12-14 13:20:49 -0800

    Functions

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - cimoutes sum of all multiples of 3 and 5 below 1024
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("\n");
+}
