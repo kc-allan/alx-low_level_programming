6f1e4fbf89520d3ed8ba3dfa89eab10faf79ed75
allan <allancheruiyot003@gmail.com>
2022-12-14 13:01:05 -0800

    Functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,22 +7,22 @@ void times_table(void)
 {
 	int line, pos, prod;
 
-	for (line = 0; line < 9; line++)
+	for (line = 0; line < 10; line++)
 	{
-		for (pos = 0; pos < 9; pos++)
+		for (pos = 0; pos < 10; pos++)
 		{
 			prod = line * pos;
-			if (prod <= 9)
+			if (prod <= 10)
 			{
 				_putchar((prod) + '0');
 				_putchar(',');
 				_putchar(' ');
-				if (line == 9)
+				if (line == 10)
 				{
 					_putchar('\n');
 				}
 			}
-			else if (line == 9)
+			else if (line == 10)
 			{
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
@@ -34,6 +34,9 @@ void times_table(void)
 				_putchar((prod % 10) + '0');
 				_putchar(',');
 				_putchar(' ');
+				if (line == 10)
+				{
+					_putchar('\n');
 			}
 		}
 	}
