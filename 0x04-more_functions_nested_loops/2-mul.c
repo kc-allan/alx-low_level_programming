3c60f3756fc6b05f639f0f55945ba27481311acd
allan <allancheruiyot003@gmail.com>
2022-12-15 10:57:08 -0800

    Functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies parameter a and b
+ * @a: First parameter
+ * @b: Second parameter
+ * Return: 1 if uppercase, 0 if lowercase
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
