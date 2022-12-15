da8d2a502acdb6bccd917e56e3fdaba1dc868af8
allan <allancheruiyot003@gmail.com>
2022-12-15 11:07:31 -0800

    Functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ * Return: void (Success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
