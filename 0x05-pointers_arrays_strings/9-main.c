027f044632ee607688d65a5701a4170722c1742d
allan <allancheruiyot003@gmail.com>
2022-12-19 12:23:07 -0800

    ALX SE

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * main - check the code
  *
@@ -6,5 +8,11 @@
  */
 int main(void)
 {
+  char s1[98];
+  char *ptr;
 
+  ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+  printf("%s", s1);
+  printf("%s", ptr);
+  return (0);
 }
