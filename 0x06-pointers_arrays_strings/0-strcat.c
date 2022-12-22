bcfa7307726cc5bf48dd95e9f0cc38e0b0cdd01a
allan <allancheruiyot003@gmail.com>
2022-12-22 10:22:48 -0800

    ALX SE

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * _strcat - Concanates two strings
- * @dest: Original string
- * @src: Appended string
+ * @*dest: Original string
+ * @*src: Appended string
  * Return: pointer to the final string
  */
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
 	int dest_len = 0;
-	
+
 	while (dest[i++])
 	{
 		dest_len++;
