bf1c3ade133f9f37a5db7798da663343593447c2
allan <allancheruiyot003@gmail.com>
2023-01-03 03:28:30 -0800

    ALX SE

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,6 +4,7 @@
  * @n: number of bytes to be filled
  * @s: points to the memory area
  * @b: constant byte to be filled
+ * Return: pointer to memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
