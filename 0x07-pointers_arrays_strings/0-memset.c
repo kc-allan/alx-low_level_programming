bddb415603ff51419e08a8bc5a3c8167b55420a1
allan <allancheruiyot003@gmail.com>
2023-01-03 03:27:03 -0800

    ALX SE

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
  * _memset - fills memory with a constant byte
  * @n: number of bytes to be filled
  * @s: points to the memory area
- * @b constant byte to be filled
+ * @b: constant byte to be filled
  */
 char *_memset(char *s, char b, unsigned int n)
 {
