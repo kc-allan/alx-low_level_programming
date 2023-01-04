521bf0077b86248ead16d03e7e765d19a654d36b
allan <allancheruiyot003@gmail.com>
2023-01-04 10:03:41 -0800

    ALX SE

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,12 +13,14 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		k = 0;
+
 		sum1 += *(a + i*size + i);
 		k++;
 	}
 	for (i = size - 1; i >= 0; i--)
 	{
 		k = size - 1;
+		
 		sum2 += *(a + i*size + size - i - 1);
 		k--;
 	}
