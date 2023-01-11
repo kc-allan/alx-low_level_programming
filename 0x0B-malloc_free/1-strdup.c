84f6158a496ce170dc8effc920ed14a8ecffa602
allan <allancheruiyot003@gmail.com>
2023-01-10 23:31:50 -0800

    ALX SE

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,12 +7,18 @@
  */
 char *_strdup(char *str)
 {
-	char *dup;
-	unsigned int i, n;
+	char *dup, *str1;
+	unsigned int i;
+	unsigned int n = 0;
 
 	if (str == 0)
 		return (0);
-	n = sizeof(str);
+	str1 = str;
+	while (*str)
+	{
+		n++;
+		str++;
+	}
 	dup = (char *)malloc(n * sizeof(char));
 	if (dup == 0)
 		return (0);
@@ -20,7 +26,7 @@ char *_strdup(char *str)
 	{
 		for (i = 0; i < n; i++)
 		{
-			*(dup + i) = *(str + i);
+			*(dup + i) = *(str1 + i);
 		}
 		return (dup);
 	}
