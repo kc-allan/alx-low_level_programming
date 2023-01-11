bed8544ce11971c51a19ff78c973eaa73ece3727
allan <allancheruiyot003@gmail.com>
2023-01-11 09:48:08 -0800

    ALX SE

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,40 +15,25 @@ char *str_concat(char *s1, char *s2)
 	dest = s1;
 	src = s2;
 	if (s1 != NULL)
-	{
 		while (*s1)
-		{
 			n++;
 			s1++;
-		}
-	}
 	else
-	{
 		n = 0;
-		s1 = "";
-	}
 	if (s2 != NULL)
 		while (*s2)
-		{
 			n++;
 			s2++;
-		}
-	else
-		s2 = "";
 	res = (char *)malloc(n * sizeof(char));
 	if (dest != NULL)
 		for (i = 0; dest[i] != '\0'; i++)
-		{
 			res[i] = dest[i];
-		}
 	else
 		i = 0;
 	if (src != NULL)
 		for (k = 0; src[k] != '\0'; k++)
-		{
 			res[i] = src[k];
 			i++;
-		}
 	res[i] = '\0';
 	return (res);
 }
