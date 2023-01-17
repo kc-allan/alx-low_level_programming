b2fe0968086c97184b4231c25d0ab7b9c4870836
allan <allancheruiyot003@gmail.com>
2023-01-17 04:10:08 -0800

    ALX SE

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG
 #define DOG
-struct dog {
+/**
+ * struct dog - structure for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Descriptiom: Structure of a dog with name, age and owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
