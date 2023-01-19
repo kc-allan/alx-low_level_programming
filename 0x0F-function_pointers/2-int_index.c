a32e51fd47693376446ecc68205c783c67b2d515
allan <allancheruiyot003@gmail.com>
2023-01-19 08:06:18 -0800

    ALX SE

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,17 +10,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, res;
 
-	if (!array || !cmp)
-		return (-1);
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		res = cmp(array[i]);
-		if (res != 0)
-			return (i);
-		if (i == size && res != 0)
-			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			res = cmp(array[i]);
+			if (res != 0)
+				return (i);
+		}
 	}
-	return (0);
+	return (-1);
 }
