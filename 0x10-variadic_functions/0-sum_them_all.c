1f6549ef0b20b8b48e687f3fbb10ffb089ec5679
allan <allancheruiyot003@gmail.com>
2023-01-20 05:40:56 -0800

    ALX SE

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,7 +11,7 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, sum = 0;
 
 	if (n == 0)
-		return(0);
+		return (0);
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
