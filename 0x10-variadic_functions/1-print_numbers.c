120bbfe5ae15da997651c902e4432a22f00c6488
allan <allancheruiyot003@gmail.com>
2023-01-20 13:56:39 -0800

    ALX SE

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,23 +10,15 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i, x;
+	unsigned int i;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		x = va_arg(args, int);
-		if (i != n - 1)
-		{
-			if (separator == NULL)
-				printf("%d", x);
-			else
-				printf("%d%s", x, separator);
-		}
-		else
-		{
-			printf("%d\n", x);
-		}
+		printf("%d", va_arg(args, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
 	}
+	printf("\n");
 	va_end(args);
 }
