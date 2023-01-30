b687c0e6d2b8bc55ce28c44afe7b002305b97a4e
allan <allancheruiyot003@gmail.com>
2023-01-30 11:08:43 -0800

    ALX SE

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - peints all elements of list_t
+ * @h: pointer to list
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int sum = 0;
+
+
+	while (h->next != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		sum++;
+		h = h->next;
+		if (h->next == NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			sum++;
+			return (sum);
+		}
+	}
+	return (0);
+}
