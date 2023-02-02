88a0cd58aaf2ce5108089a57e0df9d42804fc551
allan <allancheruiyot003@gmail.com>
2023-02-02 11:06:19 -0800

    ALX SE

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,10 +8,11 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
-	int data = (*head)->n;
+	int data;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (0);
+	data = (*head)->n;
 	temp = (*head)->next;
 	free(*head);
 	*head = temp;
