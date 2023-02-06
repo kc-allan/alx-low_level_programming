dfe14d390f27d6f37e0f07d109f4bc58291ebaf1
allan <allancheruiyot003@gmail.com>
2023-02-06 11:26:15 -0800

    ALX SE

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,18 +1,13 @@
 #include "main.h"
 /**
- * get_bit - gets bit at an index
- * @n: number in base 10
- * @index: position of bit
- * Return: bit at a position
+ * get_bit - returns the value of a bit at a given index
+ * @n: unsigned long int to search
+ * @index: index to return
+ * Return: the value of the bit at index or -1 if error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int k = 0;
-
-	if (n > 1 && k != index)
-	{
-		k++;
-		get_bit(n >> 1, index);
-	}
-	return (k);
+	if (index > 53)
+		return (-1);
+	return ((n >> index) & 1);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,19 +1,14 @@
 #include "main.h"
 /**
- * set_bit - sets bit at index to 1
- * @n: number in base 10
- * @index: position of bit
- * Return: 1(success) -1 (fail)
+  * set_bit - sets the value of a bit to 1 at a given index
+  * @n: unsigned long to change
+  * @index: index to change to one
+  * Return: 1 if it worked or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int k;
-
-	if (&n > 1 && k != index)
-	{
-		k++;
-		set_bit(n >> 1, index);
-		return (n);
-	}
-	return (-1);
+	if (index > 53 || !n)
+		return (-1);
+	(*n |= 1 << index);
+	return (1);
 }
