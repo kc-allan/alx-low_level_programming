ede43abeff2894e4176a8ba5240a353d76de3798
allan <allancheruiyot003@gmail.com>
2023-02-06 11:39:27 -0800

    ALX SE

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,28 +14,21 @@ list_t *add_node_end(list_t **head, const char *str)
 	size_t nchar = 0;
 
 	new = malloc(sizeof(list_t));
-
 	if (new == NULL)
 		return (NULL);
-
 	new->str = strdup(str);
-
 	while (str[nchar])
 		nchar++;
-
 	new->len = nchar;
 	new->next = NULL;
 	temp = *head;
-
 	if (temp == NULL)
 		*head = new;
-
 	else
 	{
 		while (temp->next != NULL)
 		temp = temp->next;
 		temp->next = new;
 	}
-
 	return (*head);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	size_t i = 0;
 
 	node = malloc(sizeof(listint_t));
-	if (!node)
+	if (node == NULL)
 		return (NULL);
 
 	node->n = n;
