b681dfa0362b338d4b572e4949ef07b59bc4d430
allan <allancheruiyot003@gmail.com>
2023-03-16 15:05:27 -0700

    ALX SE

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,8 +6,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int i;
+	int i = 0;
 
+	if (h == NULL)
+		return (i);
+	while (h->prev != NULL)
+		h = h->prev;
 	for (i = 0; h->next != NULL; i++)
 	{
 		printf("%d\n", h->n);
