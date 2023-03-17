c20b3cd4a0eff37ce39ef3264521101756d57af1
allan <allancheruiyot003@gmail.com>
2023-03-17 05:33:39 -0700

    ALX SE

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - gets nth node in list
+ * @head: pointer to list
+ * @index: index to be searched
+ * Return: nth node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; head->prev != NULL; i++)
+		head = head->prev;
+	for (i = 0; head->next != NULL; i++)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+	}
+	return (NULL);
+}
