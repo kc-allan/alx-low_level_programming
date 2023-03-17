3c048337f091584fade28c8a297a09e002102f86
allan <allancheruiyot003@gmail.com>
2023-03-17 11:58:54 -0700

    ALX SE

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deletes node at an index
+ * @head: pointer to list
+ * @imdex: position of node to be deleted
+ * Return: updated list
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i;
+
+	if (*head == NULL)
+		return (-1);
+	while ((*head)->prev != NULL)
+		*head = (*head)->prev;
+	for (i = 0; (*head)->next != NULL; i++)
+	{
+		if (i == index)
+		{
+			if ((*head)->next == NULL)
+				((*head)->prev)->next = NULL;
+			else if (index == 0)
+				((*head)->next)->prev = NULL;
+			else
+			{
+				((*head)->prev)->next = (*head)->next;
+				((*head)->next)->prev = (*head)->prev;
+			}
+			free(*head);
+		}
+		*head = (*head)->next;
+	}
+	return (1);
+}
