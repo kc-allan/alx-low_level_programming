96e43df9deb6c4865523142b9a7c0e1dceaeccaa
allan <allancheruiyot003@gmail.com>
2023-03-17 11:25:32 -0700

    ALX SE

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,50 +1,54 @@
 #include "lists.h"
+
 /**
- * insert_dnodeint_at_index - inserts node at idx
- * @h: pointer to list
- * @idx: index of new node
- * @n: element of node
- * Return: new updated list
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i, k;
-	dlistint_t *new, *temp, *count;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-	new = malloc(sizeof(dlistint_t));
-	new->n = n;
-	new->prev = NULL;
-	new->next = NULL;
+	new = NULL;
 	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
 	{
-		add_dnodeint(h, n);
-		return (*h);
-	}
-	if (*h == NULL)
-		return (new);
-	while ((*h)->prev != NULL)
-		*h = (*h)->prev;
-	count = *h;
-	for (k = 0; count->next != NULL; k++)
-		count = count->next;
-	if (k + 1 == idx || (int)idx < 0)
-	{
-		add_dnodeint_end(h, n);
-		return (*h);
-	}
-	if (idx > k + 1)
-		return (NULL);
-	for (i = 0; (*h)->next != NULL; i++)
-	{
-		if (i == idx - 1)
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
 		{
-			new->prev = *h;
-			temp = (*h)->next;
-			(*h)->next = new;
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
 		}
-		*h = (*h)->next;
 	}
-	*h = temp;
-	new->next = *h;
-	return (*h);
+
+	return (new);
 }
