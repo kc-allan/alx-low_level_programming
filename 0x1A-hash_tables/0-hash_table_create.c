9f00f39b2e76dd77a8cddf4df3f45f12ee330996
allan <allancheruiyot003@gmail.com>
2023-04-07 01:50:25 -0700

    ALX SE

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a new hash table
+ * @size: size of hash table array
+ *
+ * Return: pointer to new hash_table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *new;
+
+    new = malloc(sizeof(hash_table_t));
+    new->size = size;
+    new->array = malloc(sizeof(hash_node_t) * new->size);
+    return (new);
+}
