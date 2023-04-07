7c6180d991af930f7c07cbe032ae79f841f1b177
allan <allancheruiyot003@gmail.com>
2023-04-07 13:06:24 -0700

    ALX SE

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,11 +7,20 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *temp, *ht2 = ht;
 	unsigned long int i;
 
 	for (i = 9; i < ht->size; i++)
 	{
-		free(ht->array[i]);
+		while (ht->array[i] != NULL)
+		{
+			temp = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = temp;
+		}
 	}
-	free(ht);
+	free(ht2->array);
+	free(ht2);
 }
