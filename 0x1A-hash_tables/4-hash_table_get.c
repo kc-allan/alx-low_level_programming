7beba6fda9c9e3b4bb7598d5947979751a69c717
allan <allancheruiyot003@gmail.com>
2023-04-07 12:18:28 -0700

    ALX SE

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,9 +9,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index, i;
 
-	if (ht == NULL || key == NULL || *key || NULL)
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 	for (i = index; ht->array[i]; i++)
 	{
 		if (ht->array[i] == NULL)
