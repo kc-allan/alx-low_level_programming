12a9a30a6930d7a0cadc7348ec92cd94e0407b27
allan <allancheruiyot003@gmail.com>
2023-04-07 03:28:20 -0700

    ALX SE

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ * key_index - Comoutes a index from a hash value
+ * @key: key for data
+ * @size: size of hash table
+ *
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+	unsigned long int hash;
+
+	hash = hash_djb2(key);
+	index = hash % size;
+	return (index);
+}
