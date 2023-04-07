7b726a8ca6f0b7368836ca094231e1c36794c5da
allan <allancheruiyot003@gmail.com>
2023-04-07 02:03:48 -0700

    ALX SE

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *new;
+    unsigned long int i;
 
     new = malloc(sizeof(hash_table_t));
     if (new == NULL)
@@ -16,5 +17,9 @@ hash_table_t *hash_table_create(unsigned long int size)
     new->array = malloc(sizeof(hash_node_t) * size);
     if (new->array == NULL)
         return (NULL);
+    for (i = 0; i < size; i++)
+    {
+        new->array[i] = NULL;
+    }
     return (new);
 }
