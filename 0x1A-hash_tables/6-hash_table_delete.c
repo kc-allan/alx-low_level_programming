aee9cb5c212bf41b453cd60c1b59b72c6639ce61
allan <allancheruiyot003@gmail.com>
2023-04-09 02:56:18 -0700

    ALX SE

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -3,19 +3,21 @@
 /**
  * hash_table_delete - Deletes a hash table.
  * @ht: A pointer to a hash table.
+ *
  * Return: void
  */
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *node, *temp;
 	unsigned long int i;
-	
+
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
 			node = ht->array[i];
-			while (node != NULL){
+			while (node != NULL)
+			{
 				temp = node->next;
 				free(node->key);
 				free(node->value);
