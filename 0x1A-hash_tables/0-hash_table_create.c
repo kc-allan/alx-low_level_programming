c0451a0b2fe046b5d5e234cae0d91277f4158bce
allan <allancheruiyot003@gmail.com>
2023-04-09 02:33:28 -0700

    ALX SE

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,24 +7,24 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new;
-	unsigned long int i;
+hash_table_t *new;
+unsigned long int i;
 
-	new = malloc(sizeof(hash_table_t));
-	if (new == NULL)
-		return (NULL);
-	if ((int)size <= 0)
-		return (NULL);
-	new->array = malloc(size * sizeof(hash_node_t *));
-	if (new->array == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-	{
-		new->array[i] = NULL;
-	}
-	new->size = size;
-	return (new);
+new = malloc(sizeof(hash_table_t));
+if (new == NULL)
+	return (NULL);
+if ((int)size <= 0)
+	return (NULL);
+new->array = malloc(size * sizeof(hash_node_t *));
+if (new->array == NULL)
+{
+	free(new);
+	return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+	new->array[i] = NULL;
+}
+new->size = size;
+return (new);
 }
