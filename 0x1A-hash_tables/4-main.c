3d62aff563fd6f87e975229d5ed9b8f35ae42299
allan <allancheruiyot003@gmail.com>
2023-04-09 02:43:29 -0700

    ALX SE

--- 0x1A-hash_tables/4-main.c
+++ 0x1A-hash_tables/4-main.c
@@ -22,7 +22,12 @@ int main(void)
     hash_table_set(ht, "Betty", "Cool");
     hash_table_set(ht, "98", "Battery Street");
     hash_table_set(ht, "c", "isfun");
+	hash_table_set(ht, "Canada", "Ottawa");
 
+	value = hash_table_get(NULL, "Canada");
+	printf("%s:%s\n", "Canada", value);
+	value = hash_table_get(ht, "Canada");
+	printf("%s:%s\n", "Canada", value);
     value = hash_table_get(ht, "python");
     printf("%s:%s\n", "python", value);
     value = hash_table_get(ht, "Bob");
